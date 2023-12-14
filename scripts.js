e0c903b5420480fea2dd983f4823ed86b9becc10
Iraposeiro <101595356+iraposeiro39@users.noreply.github.com>
2023-12-14 15:32:29 +0000

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+/* Fullscreen function */
+function openFullscreen() {
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) {
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) {
+      elem.msRequestFullscreen();
+    }
+  }
