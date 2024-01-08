b94d89c5c6b955e860c9faf511bfc2856af925b5
Iraposeiro <101595356+iraposeiro39@users.noreply.github.com>
2024-01-08 14:54:26 +0000

    Changelog:

--- scripts.js
+++ scripts.js
@@ -8,3 +8,10 @@ function openFullscreen() {
       elem.msRequestFullscreen();
     }
   }
+
+/* Disable UP/DOWN arrow scroll */
+window.addEventListener("keydown", function(e) {
+  if(["Space","ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].indexOf(e.code) > -1) {
+      e.preventDefault();
+  }
+}, false);
