8bbc314dbd361536bce34dcba63ae0e8907c16ee
Iraposeiro <101595356+iraposeiro39@users.noreply.github.com>
2024-01-25 12:31:02 +0000

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -19,7 +19,7 @@ window.addEventListener("keydown", function(e) {
 var x = 1; //initializes counter
 function switchImg() {
   if (x == 1) { //if the counter is at 1
-    document.getElementById("imgToSwitch").src = "/img/afonso.png";
+    document.getElementById("imgToSwitch").src = "../img/afonso.png";
     x++; //increments the counter to 2
   } else if (x == 2) { //if this is the 2nd click;
     x = 1; //resets counter to 1
