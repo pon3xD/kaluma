8014758915241b0164de6ad27a63873d821a9118
Iraposeiro <101595356+iraposeiro39@users.noreply.github.com>
2024-01-25 12:24:30 +0000

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -15,3 +15,13 @@ window.addEventListener("keydown", function(e) {
       e.preventDefault();
   }
 }, false);
+
+var x = 1; //initializes counter
+function switchImg() {
+  if (x == 1) { //if the counter is at 1
+    document.getElementById("imgToSwitch").src = "/img/afonso.png";
+    x++; //increments the counter to 2
+  } else if (x == 2) { //if this is the 2nd click;
+    x = 1; //resets counter to 1
+  }
+}
